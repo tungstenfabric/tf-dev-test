ddd33193829a605e8c6c0c268337a18bb7534137
alexey-mr <alexey.morlang@gmail.com>
2019-11-21 21:03:58 +0300

    Added export for ssu user var

--- contrail-sanity/run.sh
+++ contrail-sanity/run.sh
@@ -7,6 +7,7 @@ source "$my_dir/../common/functions.sh"
 
 export DOMAINSUFFIX=${DOMAINSUFFIX-$(hostname -d)}
 export IMAGE_WEB_SERVER=${IMAGE_WEB_SERVER-"pnexus.sytes.net/repository/"}
+export SSH_USER=${SSH_USER:-$(whoami)}
 
 # 
 TF_TEST_NAME="contrail-test"
@@ -19,7 +20,6 @@ TF_TEST_PROJECT="Juniper/$TF_TEST_NAME.git"
 TF_TEST_TARGET=${TF_TEST_TARGET:-'ci_k8s_sanity'}
 TF_TEST_INPUT_TEMPLATE=${TF_TEST_INPUT_TEMPLATE:-"$my_dir/contrail_test_input.$ORCHESTRATOR.yaml.j2"}
 
-SSH_USER=${SSH_USER:-$(whoami)}
 
 cd $WORKSPACE
 
