e5a70bf4dc714575053a951ad594f57a99c3c69d
Arina Altynova <aaltynova@progmaticlab.com>
2020-08-07 16:16:18 +0300

    [+] python and docker installation for clear node
    Change-Id: Iafdd5ce199f85965049d3169855c98b914090b5e

--- common/functions.sh
+++ common/functions.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash +x
 
 function wait_cmd_success() {
   local cmd=$1
@@ -57,5 +57,34 @@ function set_ssh_keys() {
     sudo bash -c "$(declare -f set_ssh_keys_current_user); set_ssh_keys_current_user"
   else
     sudo runuser -u $user "$(declare -f set_ssh_keys_current_user); set_ssh_keys_current_user"
+  fi
+}
+
+function install_prerequisites_centos() {
+  local pkgs=""
+  which lsof || pkgs+=" lsof"
+  which python || pkgs+=" python"
+  if [ -n "$pkgs" ] ; then
+    sudo yum install -y $pkgs
+  fi
+}
+
+
+function install_prerequisites_rhel() {
+  RHEL_VERSION="rhel$( cat /etc/redhat-release | egrep -o "[0-9]*\." | cut -d '.' -f1 )"
+  if [[ "$RHEL_VERSION" == 'rhel8' ]] && ! which python ; then
+    which python3 || sudo yum install -y python3
+    sudo alternatives --set python /usr/bin/python3
+  fi
+  install_prerequisites_centos
+}
+
+function install_prerequisites_ubuntu() {
+  local pkgs=""
+  which lsof || pkgs+=" lsof"
+  which python || pkgs+=" python-minimal"
+  if [ -n "$pkgs" ] ; then
+    export DEBIAN_FRONTEND=noninteractive
+    sudo -E apt-get install -y $pkgs
   fi
 }
