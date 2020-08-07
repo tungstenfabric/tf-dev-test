77ed01e00dc0763357d765ffd75dac7e0c5cb6ca
Arina Altynova <aaltynova@progmaticlab.com>
2020-08-07 16:16:18 +0300

    [+] python and docker installation for clear node
    Change-Id: Iafdd5ce199f85965049d3169855c98b914090b5e

--- contrail-sanity/run.sh
+++ contrail-sanity/run.sh
@@ -15,6 +15,12 @@ if [ -z "$TF_TEST_IMAGE" ] ; then
     [ -n "$CONTAINER_REGISTRY" ] && TF_TEST_IMAGE="${CONTAINER_REGISTRY}/${TF_TEST_IMAGE}"
 fi
 
+echo '[ensure python is present]'
+install_prerequisites_$DISTRO
+
+# prepare env
+sudo -E $my_dir/../common/setup_docker.sh
+
 k8s_target='ci_k8s_sanity'
 if [[ "$DEPLOYER" == 'openshift' ]] ; then
   k8s_target='ci_openshift'
