debe71597fa7a1aed1af9382c8b45ba8dda08868
Arina Altynova <aaltynova@progmaticlab.com>
2020-08-07 16:16:18 +0300

    [+] python and docker installation for clear node
    Change-Id: Iafdd5ce199f85965049d3169855c98b914090b5e

--- common/functions.sh
+++ common/functions.sh
@@ -59,3 +59,35 @@ function set_ssh_keys() {
     sudo runuser -u $user "$(declare -f set_ssh_keys_current_user); set_ssh_keys_current_user"
   fi
 }
+
+function install_prerequisites_centos() {
+  local pkgs=""
+  which lsof || pkgs+=" lsof"
+  which python || pkgs+=" python"
+  if [ -n "$pkgs" ] ; then
+    sudo yum install -y $pkgs
+  fi
+}
+
+function install_prerequisites_rhel() {
+  RHEL_VERSION="rhel$( cat /etc/redhat-release | egrep -o "[0-9]*\." | cut -d '.' -f1 )"
+  if [[ "$RHEL_VERSION" == 'rhel8' ]] && ! which python ; then
+    if which python2 ; then
+      alternatives --set python /usr/bin/python2
+    else
+      which python3 || yum install -y python3
+      alternatives --set python /usr/bin/python3
+    fi
+  fi
+  install_prerequisites_centos
+}
+
+function install_prerequisites_ubuntu() {
+  local pkgs=""
+  which lsof || pkgs+=" lsof"
+  which python || pkgs+=" python-minimal"
+  if [ -n "$pkgs" ] ; then
+    export DEBIAN_FRONTEND=noninteractive
+    sudo -E apt-get install -y $pkgs
+  fi
+}
