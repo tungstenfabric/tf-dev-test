360b3f72fd525eb3f79574e84294aef66e11475c
Andrey Pavlov <andrey-mp@yandex.ru>
2020-09-03 08:45:34 +0300

    use built images for ziu
    Change-Id: I3468ae671fea398c75b3486bf5df89c2db54eba5

--- ziu-test/run.sh
+++ ziu-test/run.sh
@@ -6,14 +6,13 @@ my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
 export PATH=$PATH:/snap/bin
-export CONTROLLERS_COUNT=`echo "$( echo $CONTROLLER_NODES | tr ',' ' ' )" | awk -F ' ' '{print NF}'`
-export status_nodes=$(( $CONTROLLERS_COUNT * 3 ))
-export current_status=1
-i=0
+
+CONTROLLERS_COUNT=`echo "$( echo $CONTROLLER_NODES | tr ',' ' ' )" | awk -F ' ' '{print NF}'`
+status_nodes=$(( $CONTROLLERS_COUNT * 3 ))
 
 function ziu_it() {
-    local zius_it=$(juju status | grep "$1" | wc -l)
-    current_status="$(( $zius_it - $status_nodes ))"
+  local zius_it=$(juju status | grep "$1" | wc -l)
+  current_status="$(( $zius_it - $status_nodes ))"
 }
 
 function wait_cmd_success() {
@@ -38,30 +37,27 @@ juju run-action contrail-controller/leader upgrade-ziu
 
 wait_cmd_success "ziu is in progress - stage\/done = 0\/None" 10 60
 
-juju config contrail-analytics image-tag=nightly-master
-juju config contrail-analyticsdb image-tag=nightly-master
-juju config contrail-agent image-tag=nightly-master
-juju config contrail-openstack image-tag=nightly-master
-juju config contrail-controller image-tag=nightly-master
+juju config contrail-analytics image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
+juju config contrail-analyticsdb image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
+juju config contrail-agent image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
+juju config contrail-openstack image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
+juju config contrail-controller image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
 
 wait_cmd_success "ziu is in progress - stage\/done = 5\/5" 10 540
 
-i=0
-while [ $i -lt $CONTROLLERS_COUNT ]; do
-    juju run-action --wait contrail-agent/$i upgrade
-    i=$((i + 1))
+for agent in $(juju status | grep -o "contrail-agent/[0-9]*"); do
+  juju run-action --wait $agent upgrade
 done
 
 i=1
 while true; do
-    printf "."
-    status="$(juju status)"
-    if [[ $status =~ "ziu" ]]; then
-      i=$((i + 1))
-    else
-        echo -e "\nINFO:  done in $((i*10))"
-        break
-    fi
-    sleep 10
+  printf "."
+  status="$(juju status)"
+  if [[ $status =~ "ziu" ]]; then
+    i=$((i + 1))
+  else
+    echo -e "\nINFO:  done in $((i*10))"
+    break
+  fi
+  sleep 10
 done
-
