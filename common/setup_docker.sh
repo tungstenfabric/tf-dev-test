bb98860449fcf3718376bb6cf6c47eba7dc6e124
Arina Altynova <aaltynova@progmaticlab.com>
2020-09-10 01:38:56 +0300

    [+] python and docker installation for clear node [podman]
    merge conflict fix 2
    
    Change-Id: I743e1505ce3a105d84ea5f7d39b7fa71d282f00c

--- common/setup_docker.sh
+++ common/setup_docker.sh
@@ -1,42 +1,29 @@
 #!/bin/bash -e
-
+set -x
 # do not source common.sh - it sources stack profile and breaks variables are set after the first call
+scriptdir=$(realpath $(dirname "$0"))
+source ${scriptdir}/functions.sh
+
+### basic functions
 
 function retry() {
     local i
     for ((i=0; i<5; ++i)) ; do
-        if $@ ; then
-            break
-        fi
+        $@ && break
         sleep 5
     done
-    if [[ $i == 5 ]]; then
-        return 1
+    [[ $i == 5 ]] && return 1
+}
+
+function is_registry_insecure() {
+    echo "DEBUG: is_registry_insecure: $@"
+    local registry=`echo $1 | sed 's|^.*://||' | cut -d '/' -f 1`
+    if  curl -s -I --connect-timeout 60 http://$registry/v2/ ; then
+        echo "DEBUG: is_registry_insecure: $registry is insecure"
+        return 0
     fi
-}
-
-function install_docker_ubuntu() {
-    export DEBIAN_FRONTEND=noninteractive
-    which docker && return
-    retry apt-get update
-    retry apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    add-apt-repository -y -u "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    retry apt-get install -y "docker-ce=18.06.3~ce~3-0~ubuntu"
-}
-
-function install_docker_centos() {
-    which docker && return
-    yum install -y yum-utils device-mapper-persistent-data lvm2
-    if ! yum info docker-ce &> /dev/null ; then
-        yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-    fi
-    retry yum install -y docker-ce-18.03.1.ce
-}
-
-function install_docker_rhel() {
-    which docker && return
-    sudo yum install -y docker device-mapper-libs device-mapper-event-libs
+    echo "DEBUG: is_registry_insecure: $registry is secure"
+    return 1
 }
 
 function check_docker_value() {
@@ -54,83 +41,54 @@ function ensure_root() {
     fi
 }
 
-function is_registry_insecure() {
-    echo "DEBUG: is_registry_insecure: $@"
-    local registry=`echo $1 | sed 's|^.*://||' | cut -d '/' -f 1`
-    if  curl -s -I --connect-timeout 60 http://$registry/v2/ ; then
-        echo "DEBUG: is_registry_insecure: $registry is insecure"
-        return 0
-    fi
-    echo "DEBUG: is_registry_insecure: $registry is secure"
-    return 1
+### install_docker_DISTRO functions
+
+function install_docker_ubuntu() {
+    export DEBIAN_FRONTEND=noninteractive
+    retry apt-get update
+    retry apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository -y -u "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    retry apt-get install -y "docker-ce=18.06.3~ce~3-0~ubuntu"
 }
 
-ensure_root
-
-echo ""
-echo 'INFO: [docker install]'
-echo "INFO: distro=$DISTRO detected"
-
-if ! which docker >/dev/null 2>&1 ; then
-    if [ x"$DISTRO" == x"centos" ]; then
-        systemctl stop firewalld || true
-        install_docker_centos
-        systemctl start docker
-    elif [ x"$DISTRO" == x"rhel" ]; then
-        if [ "$RHEL_VERSION" == "rhel8" ]; then
-            echo "ERROR: we don't support rhel8 now"
-            exit 1
-        fi
-        systemctl stop firewalld || true
-        install_docker_rhel
-        systemctl start docker
-    elif [ x"$DISTRO" == x"ubuntu" ]; then
-        install_docker_ubuntu
+function install_docker_centos() {
+    yum install -y yum-utils device-mapper-persistent-data lvm2
+    if ! yum info docker-ce &> /dev/null ; then
+        yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     fi
-else
-  echo "INFO: docker installed: $(docker --version)"
-fi
+    retry yum install -y docker-ce-18.03.1.ce
+}
 
-echo
-echo '[docker config]'
+function install_docker_rhel() {
+    if [ "$RHEL_VERSION" != "rhel8" ]; then
+        sudo yum install -y docker device-mapper-libs device-mapper-event-libs
+    else
+        sudo yum install -y podman device-mapper-libs device-mapper-event-libs
+        sudo ln -s "$(which podman)" /usr/bin/docker
+    fi
+}
 
-if [[ -z "${CONTAINER_REGISTRY}" ]]; then
-    echo "INFO: CONTAINER_REGISTRY is not defined. skip docker configuration"
-    exit
-elif ! is_registry_insecure "$CONTAINER_REGISTRY" ; then
-    echo "INFO: registry ${CONTAINER_REGISTRY} is secure. skip docker configuration"
-    exit
-fi
+### configure_insecure_registries_[/etc directory]
 
-if [ -e "/etc/sysconfig/docker" ]; then
-    INSECURE_REGISTRIES=$(cat /etc/sysconfig/docker | awk -F '=' '/^INSECURE_REGISTRY=/{print($2)}' | tr -d '"')
-    echo "INFO: current /etc/sysconfig/docker insecure_registries=$INSECURE_REGISTRIES"
-else
-    INSECURE_REGISTRIES=""
-fi
+function configure_insecure_registries_containers() {
+    local insecure_registries="$(sed -n '/registries.insecure/{n; s/registries = //p}' "$DOCKER_CONFIG" | tr -d '[]')"
+    echo "INFO: old registries are $insecure_registries"
+    insecure_registries="registries =[$insecure_registries, '$1']"
+    echo "INFO: new registries are $insecure_registries"
+    sudo sed -i "/registries.insecure/{n; s/registries = .*$/${insecure_registries}/g}" ${DOCKER_CONFIG}
+}
 
-if [ -z "$INSECURE_REGISTRIES" ]; then
-    DOCKER_CONFIG="/etc/docker/daemon.json" 
-else
-    DOCKER_CONFIG="/etc/sysconfig/docker"
-fi
-
-if [ -e $DOCKER_CONFIG ] && grep -q $CONTAINER_REGISTRY $DOCKER_CONFIG; then
-    echo "INFO: Registry $CONTAINER_REGISTRY is already configured in $DOCKER_CONFIG. skip docker configuration"
-    exit
-fi
-
-if [ ! -e $DOCKER_CONFIG ]; then
-    touch $DOCKER_CONFIG
-fi
-
-echo "INFO: fix $DOCKER_CONFIG"
-if [ "$DOCKER_CONFIG" == "/etc/sysconfig/docker" ]; then
-    INSECURE_REGISTRIES+=" --insecure-registry $CONTAINER_REGISTRY"
+function configure_insecure_registries_sysconfig() {
+    echo "INFO: add REGISTRY=$1 to insecure list"
+    local insecure_registries=$(cat /etc/sysconfig/docker | awk -F '=' '/^INSECURE_REGISTRY=/{print($2)}' | tr -d '"')
+    insecure_registries+=" --insecure-registry $1"
     sudo sed -i '/^INSECURE_REGISTRY/d' "$DOCKER_CONFIG"
-    echo "INSECURE_REGISTRY=\"$INSECURE_REGISTRIES\""  | sudo tee -a "$DOCKER_CONFIG"
+    echo "INSECURE_REGISTRY=\"$insecure_registries\""  | sudo tee -a "$DOCKER_CONFIG"
     sudo cat "$DOCKER_CONFIG"
-else # [ "$DOCKER_CONFIG" == "/etc/docker/daemon.json" ]; then
+}
+
+function configure_insecure_registries_docker() {
     python3 <<EOF
 import json
 data=dict()
@@ -140,13 +98,65 @@ try:
 except Exception:
   pass
 
-data.setdefault('insecure-registries', list()).append("${CONTAINER_REGISTRY}")
+data.setdefault('insecure-registries', list()).append("${1}")
 
 data['live-restore'] = True
 with open("${DOCKER_CONFIG}", 'w') as f:
   data = json.dump(data, f, sort_keys=True, indent=4)
 EOF
+}
+
+### end of function's declaration
+
+ensure_root
+
+echo ""
+echo 'INFO: [docker install]'
+echo "INFO: distro=$DISTRO detected"
+
+if ! which docker >/dev/null 2>&1 ; then
+    [ "$DISTRO" == "ubuntu" ] || systemctl stop firewalld || true
+    install_docker_$DISTRO
+    [ "$DISTRO" == "ubuntu" ] || [ "$RHEL_VERSION" == "rhel8" ] || systemctl start docker
+else
+  echo "INFO: docker installed: $(docker --version)"
 fi
+
+echo
+echo '[docker config]'
+
+# CONTAINER_REGISTRY's checks for definition and secureness
+if [ -z "${CONTAINER_REGISTRY}" ]; then
+    echo "${CONTAINER_REGISTRY} is not defined."
+    exit
+fi
+
+if ! is_registry_insecure "$CONTAINER_REGISTRY"; then
+    echo "$CONTAINER_REGISTRY registry is secure"
+    exit
+fi
+
+# finding out DOCKER_CONFIG file for insecure registries
+if [ "$RHEL_VERSION" == "rhel8" ]; then
+    DOCKER_CONFIG="/etc/containers/registries.conf"
+else
+    if [ -e "/etc/sysconfig/docker" ]; then
+        insecure_registries=$(cat "/etc/sysconfig/docker" | awk -F '=' '/^INSECURE_REGISTRY=/{print($2)}' | tr -d '"')
+        echo "INFO: current /etc/sysconfig/docker insecure_registries=$insecure_registries"
+    fi
+    DOCKER_CONFIG=$([[ -n $insecure_registries ]] && echo "/etc/sysconfig/docker" || echo "/etc/docker/daemon.json" )
+fi
+
+# if DOCKER_CONFIG already contains CONTAINER_REGISTRY --> exit
+if [ -e $DOCKER_CONFIG ] && grep -q $CONTAINER_REGISTRY $DOCKER_CONFIG; then
+    echo "Registry $CONTAINER_REGISTRY is configured in $DOCKER_CONFIG. Skip."
+    exit 0
+fi
+
+[ -e $DOCKER_CONFIG ] || touch $DOCKER_CONFIG
+
+# add CONTAINER_REGISTRY into DOCKER_CONFIG
+configure_insecure_registries_$(echo "$DOCKER_CONFIG" | cut -d "/" -f3 ) "$CONTAINER_REGISTRY"
 
 echo ""
 echo "INFO: [restart docker]"
