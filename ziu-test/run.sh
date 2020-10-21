bcf5ce497f2c5c3ee7be6f5e2219f365c13e346e
Arina Argentova <aargentova@progmaticlab.com>
2020-10-21 13:53:04 +0300

    [~]  ziu-test adapted for juju without contrail-analyticsdb charm
    Change-Id: I0abc5c4f454f888b70f62d5718074e65de440047

--- ziu-test/run.sh
+++ ziu-test/run.sh
@@ -8,64 +8,43 @@ my_dir="$(dirname $my_file)"
 export PATH=$PATH:/snap/bin
 
 CONTROLLERS_COUNT=`echo "$( echo $CONTROLLER_NODES | tr ',' ' ' )" | awk -F ' ' '{print NF}'`
-status_nodes=$(( $CONTROLLERS_COUNT * 3 ))
+analyticsdb_enabled=$(( $(juju status | cut -d " " -f1 | grep -q contrail-analyticsdb; echo $?) == 0 ))
+status_nodes=$(( $CONTROLLERS_COUNT * (2 + $analyticsdb_enabled) ))
 
-function ziu_it() {
-  local zius_it=$(juju status | grep "$1" | wc -l)
-  current_status="$(( $zius_it - $status_nodes ))"
+function ziu_status() {
+    (( $(juju status | grep "$1" | wc -l) - $status_nodes ))
 }
 
 function wait_cmd_success() {
-  i=0
-  current_status=1
-
-  while [ $current_status -ne 0 ]; do
-    printf "."
-    i=$((i + 1))
-    if (( i > $3 )) ; then
-      echo -e "\nERROR: wait failed in $((i*$2))s"
-      exit 1
-    fi
-    sleep $2
-    ziu_it "$1"
-  done
-
-  echo -e "\nINFO: done in $((i*$2))s"
+    i=0
+    while eval $3; do
+        sleep $1
+        printf "."
+        i=$((i + 1))
+        if (( i >= $2 )); then
+            echo -e "\nERROR: wait failed in $((i*$1))s"
+            exit 1
+        fi
+    done
+    echo -e "\nINFO: done in $((i*$1))s"
 }
 
 juju run-action contrail-controller/leader upgrade-ziu
 
-wait_cmd_success "ziu is in progress - stage\/done = 0\/None" 10 60
+wait_cmd_success 10 60 "ziu_status \"ziu is in progress - stage\/done = 0\/None\""
 
 juju config contrail-analytics image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
-juju config contrail-analyticsdb image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
+(( $analyticsdb_enabled )) && juju config contrail-analyticsdb image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
 juju config contrail-agent image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
 juju config contrail-openstack image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
 juju config contrail-controller image-tag=$CONTRAIL_CONTAINER_TAG docker-registry=$CONTAINER_REGISTRY
 
-wait_cmd_success "ziu is in progress - stage\/done = 5\/5" 10 540
+wait_cmd_success 20 540 "ziu_status \"ziu is in progress - stage\/done = 5\/5\""
 
 for agent in $(juju status | grep -o "contrail-agent/[0-9]*"); do
-  juju run-action --wait $agent upgrade
+    juju run-action --wait $agent upgrade
 done
 
-i=1
-while true; do
-  printf "."
-  status="$(juju status)"
-  if [[ $status =~ "ziu" ]]; then
-    i=$((i + 1))
-    if (( i > 62 )) ; then
-      echo "ERROR: ziu is still in progress after 120s"
-      exit 1
-    fi
-  else
-    echo -e "\nINFO:  done in $((i*10))"
-    break
-  fi
-  sleep 10
-done
+wait_cmd_success 10 60 "juju status | grep -q \"ziu\""
+wait_cmd_success 20 60 "juju status | grep -q \"waiting\|blocked\|maintenance\|unknown\""
 
-# to wait while all agents come back and show active state (otherwise we catch sometimes NTP state unsynchronised)
-# TODO: change to something like wait_for_active
-sleep 120
